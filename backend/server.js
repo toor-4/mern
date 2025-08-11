48c94599f391c53156def994baeb923fec8cdd14
alfred <frealchar@gmail.com>
2025-08-11 18:54:13 +0800

    feat(server): create CRUD operations for workouts

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
+const morgan = require('morgan');
 
 const connectDB = require('./config/db.js');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World, from Node.js!' });
-});
+const workoutRoutes = require('./routes/workoutRoutes.js');
+
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use('/api/workouts', workoutRoutes);
 
 app.listen(port, () => {
   connectDB();
