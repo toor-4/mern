cf624236674692f25c410d5af02491a8ba7709ab
alfred <frealchar@gmail.com>
2025-08-11 15:13:53 +0800

    fix(server-error): modify server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+
+const connectDB = require('./config/db.js');
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World, from Node.js!' });
 });
 
 app.listen(port, () => {
+  connectDB();
   //   console.log('server started at http://localhost:' + port);
   console.log(`server started at http://localhost:${port}`);
 });
